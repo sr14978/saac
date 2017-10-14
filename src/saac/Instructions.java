8cc08f0cf3f01ff6d932ba939d895e1de99ce442
Samuel Russell <sr14978@bristol.ac.uk>
2017-10-14 02:49:16 +0100

    Defined instructions

--- src/saac/Instructions.java
+++ src/saac/Instructions.java
@@ -6,24 +6,24 @@ import java.util.Map;
 
 public class Instructions {
 	enum Opcode {
-		Nop,
-		Ldc,
-		Add,
-		Addi,
-		Sub,
-		Subi,
-		Mul,
-		Muli,
-		Div,
-		Divi,
-		Ldma,
-		Stma,
-		Ldmi,
-		Stmi,
-		Br,
-		Jmp,
-		JmpZ,
-		JmpN
+		Nop,	// Nop	.  .  .  -> 
+		Ldc, 	// Ldc 	rI #n .  -> rI = #n  
+		Add, 	// Add 	rI rJ rK -> rI = rJ + rK
+		Addi, 	// Addi	rI rJ #n -> rI = rJ + #n
+		Sub, 	// Sub	rI rJ rK -> rI = rJ - rK
+		Subi, 	// Subi	rI rJ #n -> rI = rJ - #n
+		Mul, 	// Mul	rI rJ rK -> rI = rJ * rK
+		Muli, 	// Muli	rI rJ #n -> rI = rJ * #n
+		Div, 	// Div	rI rJ rK -> rI = rJ / rK
+		Divi, 	// Divi	rI rJ #n -> rI = rJ / #n
+		Ldma,	// Ldma	rI #n .  -> rI = mem[#m] 
+		Stma,	// Stma	rI #n .  -> mem[#m] = rI 
+		Ldmi,	// Ldmi	rI rJ rK -> rI = mem[rJ + rk] 
+		Stmi,	// Stmi	rI rJ rK -> mem[rJ + rk] = rI 
+		Br,		// Br 	#n .  .  -> pc = #n
+		Jmp,	// Jmp  #n .  .  -> pc = pc + #n
+		JmpZ,	// Jmp  #n rI .  -> pc = if rI=0 then pc + #n else pc
+		JmpN	// Jmp  #n rI .  -> pc = if rI<0 then pc + #n else pc
 	}
 	static Map<Opcode, Integer> InstructionDelay = new HashMap<Opcode, Integer>(){
 		private static final long serialVersionUID = 1L;
